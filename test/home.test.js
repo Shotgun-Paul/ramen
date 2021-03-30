fe57eb75045d9491930933250729da27aa857de4
Павел Павлов <criusmail@gmail.com>
2021-03-30 11:25:52 +0600

    Three items are added to the cart. Raw and slow code

--- test/home.test.js
+++ test/home.test.js
@@ -30,16 +30,43 @@ describe('Home page scenarios', function () {
         await page.driver.sleep(3000);
     });
 
-    it('Test check', async function () {
+    it('Test check 1', async function () {
         await driver.wait(until.elementLocated(By.css('[data-product-lid="1600412736308"] > a.js-product-link')), 15000);
         var ramen1 = await driver.findElement(By.css('[data-product-lid="1600412736308"] > a.js-product-link'));
         
-        await page.driver.sleep(3000);
+        await page.driver.sleep(3000); // kolhoz
         await ramen1.click();
 
-        console.log();
+        await driver.wait(until.elementLocated(By.css('#t754__product-1600412736308 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a')), 15000);
+        var addToCart = await driver.findElement(By.css('#t754__product-1600412736308 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a'));
+
+        await addToCart.click();
+    });
+
+    it('Test check 2', async function () {
+        await driver.wait(until.elementLocated(By.css('[data-product-lid="1600672755644"] > a.js-product-link')), 15000);
+        var ramen1 = await driver.findElement(By.css('[data-product-lid="1600672755644"] > a.js-product-link'));
+        
+        await page.driver.sleep(3000); // kolhoz
+        await ramen1.click();
+
+        await driver.wait(until.elementLocated(By.css('#t754__product-1600672755644 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a')), 15000);
+        var addToCart = await driver.findElement(By.css('#t754__product-1600672755644 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a'));
+
+        await addToCart.click();
+    });
+
+    it('Test check 3', async function () {
+        await driver.wait(until.elementLocated(By.css('[data-product-lid="1601916310521"] > a.js-product-link')), 15000);
+        var ramen1 = await driver.findElement(By.css('[data-product-lid="1600672755644"] > a.js-product-link'));
+        
+        await page.driver.sleep(3000); // kolhoz
+        await ramen1.click();
+
+        await driver.wait(until.elementLocated(By.css('#t754__product-1601916310521 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a')), 15000);
+        var addToCart = await driver.findElement(By.css('#t754__product-1601916310521 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a'));
+        
+        await addToCart.click();
     });
 
 });
-
-// #t754__product-1600412736308 > div > div.t754__col_right.t754__wrapper.t-col.t-col_6.t-align_left > div.t754__btn-wrapper > a
